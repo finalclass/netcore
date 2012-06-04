aaa1e91e79f7d1ca2abbcd42e34b4037c7e132eb
Szymon Wygnanski <m@finalclass.net>
2012-06-04 14:18:56 +0200

    added csv import/export class

---
+++ Utils/Csv.php
@@ -0,0 +1,70 @@
+<?php
+/**
+
+Copyright (C) Szymon Wygnanski (s@finalclass.net)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ *
+ */
+namespace NetCore\Utils;
+/**
+ * @author: Sel <s@finalclass.net>
+ * @date: 04.06.12
+ * @time: 13:52
+ */
+class Csv
+{
+
+    /**
+     * @static
+     * @param $array
+     * @param array $header
+     * @param string $separator
+     * @return string
+     */
+    static public function arrayToCsv($array, $header = array(), $separator = ',')
+    {
+        $csv = array();
+        if(!empty($header)) {
+            $csv[] = join($separator, $header);
+        }
+        foreach ($array as $item) {
+            $row = array();
+            foreach($item as $field) {
+                if(!is_string($field)) {
+                    $field = '';
+                }
+                $row[] = $field;
+            }
+            $csv[] = join($separator, $row);
+        }
+        return join(PHP_EOL, $csv);
+    }
+
+    static public function csvToArray($data)
+    {
+        $instream = fopen("php://temp", 'r+');
+        fwrite($instream, $data);
+        rewind($instream);
+        $csv = fgetcsv($instream, 9999999, ',', '"');
+        fclose($instream);
+        return ($csv);
+    }
+
+}
